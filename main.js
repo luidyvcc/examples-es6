5e8fe726b7924bf74cc238dd0af062d043756063
Luidy Vaccaro <ldy80@outlook.com>
2019-09-26 08:26:16 -0300

    lesson 01.04 done

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro() {
-
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = "Luidy";
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+// Testes:
+
+var MinhaLista = new TodoList();
+
+document.getElementById("novotodo").onclick = function() {
+    MinhaLista.add("Novo todo 2");
+};
+
+MinhaLista.mostraUsuario();
+
+console.log(Matematica.soma(1, 5));
