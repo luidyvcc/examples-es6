8cdf34d0a9ef921111fedcf56f6014182080948b
Luidy Vaccaro <ldy80@outlook.com>
2019-09-26 09:11:12 -0300

    lesson 01.05 done

--- main.js
+++ main.js
@@ -1,40 +1,64 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
+// class List {
+//     constructor() {
+//         this.data = [];
+//     }
 
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
-    }
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
+// }
+
+// class TodoList extends List {
+//     constructor() {
+//         super();
+
+//         this.usuario = "Luidy";
+//     }
+
+//     mostraUsuario() {
+//         console.log(this.usuario);
+//     }
+// }
+
+// class Matematica {
+//     static soma(a, b) {
+//         return a + b;
+//     }
+// }
+
+// // Testes:
+
+// var MinhaLista = new TodoList();
+
+// document.getElementById("novotodo").onclick = function() {
+//     MinhaLista.add("Novo todo 2");
+// };
+
+// MinhaLista.mostraUsuario();
+
+// console.log(Matematica.soma(1, 5));
+
+
+// 01.04
+
+//const a = 1;
+
+//a = 2;
+
+const b = { nome : "Marcos" };
+console.log(b.nome);
+b.nome = "José";
+console.log(b.nome);
+
+if (1) {
+    var y = 4;
+    let z = 4;
 }
 
-class TodoList extends List {
-    constructor() {
-        super();
-
-        this.usuario = "Luidy";
-    }
-
-    mostraUsuario() {
-        console.log(this.usuario);
-    }
+function teste() {
+    console.log(y); // funciona
+    console.log(z); // gera um erro
 }
 
-class Matematica {
-    static soma(a, b) {
-        return a + b;
-    }
-}
-
-// Testes:
-
-var MinhaLista = new TodoList();
-
-document.getElementById("novotodo").onclick = function() {
-    MinhaLista.add("Novo todo 2");
-};
-
-MinhaLista.mostraUsuario();
-
-console.log(Matematica.soma(1, 5));
+teste();
