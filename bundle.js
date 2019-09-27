7677532c1dd6a25113d91699fd21e7a47d65082c
Luidy Vaccaro <ldy80@outlook.com>
2019-09-27 10:44:44 -0300

    lesson 01.05 done

--- bundle.js
+++ bundle.js
@@ -1,5 +1,8 @@
 "use strict";
 
+/*
+    01.03 - Classes
+*/
 // class List {
 //     constructor() {
 //         this.data = [];
@@ -30,25 +33,43 @@
 // };
 // MinhaLista.mostraUsuario();
 // console.log(Matematica.soma(1, 5));
-// 01.04
+
+/*
+    01.04 - Const & Let
+*/
 //const a = 1;
-//a = 2;
-var b = {
-  nome: "Marcos"
-};
-console.log(b.nome);
-b.nome = "José";
-console.log(b.nome);
+//a = 2; // Gera um erro
+// const b = { nome : "Marcos" };
+// console.log(b.nome);
+// b.nome = "José";
+// console.log(b.nome);
+// if (1) {
+//     var y = 4;
+//     let z = 4;
+// }
+// function teste() {
+//     console.log(y); // funciona
+//     console.log(z); // gera um erro
+// }
+// teste();
 
-if (1) {
-  var y = 4;
-  var _z = 4;
-}
-
-function teste() {
-  console.log(y); // funciona
-
-  console.log(z); // gera um erro
-}
-
-teste();
+/*
+    01.05 - Operações com array
+*/
+var arr = [1, 3, 4, 5, 8, 9];
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item == 3;
+});
+console.log(find);
--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*
+    01.03 - Classes
+*/
+
 // class List {
 //     constructor() {
 //         this.data = [];
@@ -40,25 +44,54 @@
 // console.log(Matematica.soma(1, 5));
 
 
-// 01.04
+/*
+    01.04 - Const & Let
+*/
 
 //const a = 1;
 
-//a = 2;
+//a = 2; // Gera um erro
 
-const b = { nome : "Marcos" };
-console.log(b.nome);
-b.nome = "José";
-console.log(b.nome);
+// const b = { nome : "Marcos" };
+// console.log(b.nome);
+// b.nome = "José";
+// console.log(b.nome);
 
-if (1) {
-    var y = 4;
-    let z = 4;
-}
+// if (1) {
+//     var y = 4;
+//     let z = 4;
+// }
 
-function teste() {
-    console.log(y); // funciona
-    console.log(z); // gera um erro
-}
+// function teste() {
+//     console.log(y); // funciona
+//     console.log(z); // gera um erro
+// }
 
-teste();
+// teste();
+
+
+/*
+    01.05 - Operações com array
+*/
+
+const arr = [1, 3, 4, 5, 8, 9];
+
+const newArr = arr.map(function(item, index){
+    return item + index;
+});
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+});
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item == 3;
+});
+console.log(find);
